d5f73f1e8bbbb01a7074e98f1e4e63e15c2a8d56
David Aguirre <davidaguirre@csilvm-13.cs.ucsb.edu>
2026-01-15 18:39:16 -0800

    fixed pushfront pushback

--- intlist.cpp
+++ intlist.cpp
@@ -34,10 +34,10 @@ IntList::IntList(const IntList& source) {
 
 // destructor deletes all nodes
 IntList::~IntList() {
-    while(head!=nullptr)
+    while(head!=nullptr) //goes through the list
     {
-        Node* temp = head;
-        head = head->next;
+        Node* temp = head; //create temporary pointer
+        head = head->next; //move head pointer to delete data at temporary address
         delete temp;
     }
 }
@@ -45,40 +45,109 @@ IntList::~IntList() {
 
 // return sum of values in list
 int IntList::sum() const {
-    return 0; // REPLACE THIS NON-SOLUTION
+    int sum = 0;
+    Node* current = head;
+
+    while(current!=nullptr)
+    {
+        sum += current->info;
+        current = current->next;
+    }
+    return sum;
 }
 
 // returns true if value is in the list; false if not
 bool IntList::contains(int value) const {
-    return false; // REPLACE THIS NON-SOLUTION
+    Node*current = head;
+    while(current!=nullptr)
+    {
+        if(current->info==value)
+        {
+            return true;
+        }
+        current = current->next;
+    }
+    return false;
 }
 
 // returns maximum value in list, or 0 if empty list
 int IntList::max() const {
-    return 0; // REPLACE THIS NON-SOLUTION
+    Node* current = head;
+    int max = current->info;
+
+    while(current!=nullptr)
+    {
+        if(max<current->info)
+        {
+            max = current->info;
+        }
+        current = current->next;
+    }
+    return max;
 }
 
 // returns average (arithmetic mean) of all values, or
 // 0 if list is empty
 double IntList::average() const {
-    return 0.0; // REPLACE THIS NON-SOLUTION
+    double sum = 0.0;
+    Node* current = head;
+
+    if(current==nullptr)
+    {
+        return 0;
+    }
+    int count = 0;
+    while(current!=nullptr)
+    {
+        sum += current->info;
+        count += 1;
+        current = current->next;
+    }
+    return sum/count;
 }
 
 // inserts value as new node at beginning of list
 void IntList::push_front(int value) {
     // IMPLEMENT
+    Node* newHead = new Node;
+    newHead->info = value;
+    newHead->next = head;
+    head = newHead;
+
+    if(tail==nullptr)
+    {
+        tail = newHead;
+    }
 }
 
 // append value at end of list
 void IntList::push_back(int value) {
     // IMPLEMENT
- 
+    Node* newEnd = new Node;
+    newEnd->info = value;
+    newEnd->next = nullptr;
+
+    if(!head)
+    {
+        head = newEnd;
+        tail = newEnd;
+    }
+    else{
+        tail->next = newEnd;
+        tail = newEnd;
+    }
 }
 
 // return count of values
 int IntList::count() const {
-   //IMPLEMENT THIS
-   return 0;
+   int count = 0;
+   Node* current = head;
+   while(current!=nullptr)
+   {
+    count++;
+    current = current->next;
+   }
+   return count;
 }
 
 
@@ -86,12 +155,48 @@ int IntList::count() const {
 //to this list, deleting/replacing any existing nodes
 IntList& IntList::operator=(const IntList& source){
     //IMPLEMENT
+    if(this == &source) //self-assignment check (why?)
+    {
+        return *this;
+    }
+    while(head!=nullptr) //delete existing list
+    {
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    head = nullptr; // empty list
+
+    if(source.head == nullptr) //checks for empty list at source
+    {
+        return *this;
+    }
+
+    head = new Node;
+    head->info = source.head->info;
+    head->next = nullptr;
+
+    Node* current = head; // create two currents, to create deep copies
+    Node* sourceCurrent = source.head->next;
+
+    while(sourceCurrent!=nullptr) //goes through the list and copies elements from source to current.
+    {
+        current->next = new Node;
+        current = current->next;
+        current->info = sourceCurrent->info;
+        current->next = nullptr;
+
+        sourceCurrent = sourceCurrent->next;
+    }
+
     return *this;
 }
 
 // constructor sets up empty list
 IntList::IntList(){ 
-    //IMPLEMENT THIS 
+    //IMPLEMENT THIS
+    head = nullptr;
 }
 
 
